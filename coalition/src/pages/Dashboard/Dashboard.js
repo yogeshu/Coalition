c74f647cf0cc39193b4d4dd1fc7285b985b1bc10
Yogesh Bhavsar <32289745+yogeshu@users.noreply.github.com>
2024-07-01 08:03:55 +0000

    Added sidebar, patientInfo and maincontent

--- coalition/src/pages/Dashboard/Dashboard.js
+++ coalition/src/pages/Dashboard/Dashboard.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import styles from './Dashboard.module.css'
 import Navbar from '../../components/ui/Navbar/Navbar'
+import Sidebar from '../../components/ui/Sidebar/Sidebar'
+import MainContent from '../../components/ui/MainContent/MainContent'
+import PatientInfo from '../../components/ui/PatientInfo/PatientInfo'
 
 function Dashboard() {
   return (
-    <div>Dashboard
+    <div className={styles.container}>
 
 
-        <br/>
+        
         <Navbar/>
+        <div className={styles.content}>
+          <Sidebar/>
+          <MainContent/>
+          <PatientInfo/>
+        </div>
 
     </div>
   )
